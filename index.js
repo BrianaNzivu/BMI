f984763ea89be69dd6d3dc3a1b169ff1c0ca27c5
BrianaNzivu <bjnzivu@gmail.com>
2023-07-14 10:53:47 +0300

    Fixed stuff part 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname)+"/views");
@@ -48,5 +48,6 @@ app.post('/records', jsonParser, (request, response) => {
 
 
 app.listen(port);
-console.log('server is listening to port 3300');
+console.log(`server listening on port ${port}`);
+
 
